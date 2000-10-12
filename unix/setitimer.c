e2439f99ab4085412411cc04b948ae2f7f8faa90
Frank Naumann <fnaumann@boerde.de>
2000-10-12 11:33:26 +0000

    Corrected setitimer stub. microseconds <-> milliseconds calculations for kernel binding are incorrect.

--- unix/setitimer.c
+++ unix/setitimer.c
@@ -31,27 +31,30 @@ __setitimer (enum __itimer_which which, const struct itimerval* new,
 
   if (new) 
     {
-      new_interval = (new->it_interval.tv_sec * 1000 + 
-          new->it_interval.tv_usec / 1000);
-      new_value = (new->it_value.tv_sec * 1000
-	  + new->it_value.tv_usec / 1000);
+      new_interval = new->it_interval.tv_sec * 1000;
+      new_interval += ((new->it_interval.tv_usec + 999) / 1000);
+      new_value = new->it_value.tv_sec * 1000;
+      new_value += ((new->it_value.tv_usec + 999) / 1000);
     }
+
   retval = Tsetitimer (which, new ? (long int) &new_interval : 0L, 
                               new ? (long int) &new_value : 0L,
                               old ? (long int) &old_interval : 0L, 
                               old ? (long int) &old_value : 0L);
-  if (retval < 0) 
+  if (retval < 0)
     {
-      errno = -retval;
+      __set_errno(-retval);
       return -1;
     }
 
-  if (old) 
+  if (old)
     {
-      old->it_interval.tv_sec = old_interval/1000;
+      old->it_interval.tv_sec = old_interval / 1000;
+      old->it_interval.tv_usec = (old_interval % 1000) * 1000;
+      old->it_value.tv_sec = old_value / 1000;
       old->it_value.tv_usec = (old_value % 1000) * 1000;
     }
+
   return 0;
 }
-
 weak_alias (__setitimer, setitimer)
