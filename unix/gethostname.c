e2e5ce672a6b874eb721fa1ca3335e55c27fd139
Guido Flohr <guido.flohr@cantanea.com>
2000-12-13 19:09:47 +0000

    * Finetuning * Use weak aliases (non-standard!) * Use __set_errno

--- unix/gethostname.c
+++ unix/gethostname.c
@@ -10,71 +10,71 @@
 
 /* Changed by Guido Flohr:  Include sys/param.h for maximum hostname
    length.  */
-#ifdef __TURBOC__
-# include <sys\param.h>
-# include <sys\types.h>
-#else
-# include <sys/param.h>
-# include <sys/types.h>
-#endif
+#include <sys/param.h>
+#include <sys/types.h>
 
 int
-gethostname(buf, len)
+__gethostname(buf, len)
 	char *buf;
 	size_t len;
 {
-	char *foo = 0;
-	char xbuf[MAXHOSTNAMELEN + 1];
-	int fd, r;
-  size_t real_length;
-  
-#if 0
-	if (!foo) {
-#endif
+    char *foo = 0;
+    char xbuf[MAXHOSTNAMELEN + 1];
+    int fd, r;
+    size_t real_length;
+    
+    if (!buf) {
+	__set_errno (EINVAL);
+	return -1;
+    }
+
 /* try looking for the file /etc/hostname; if it's present,
  * it contains the name, otherwise we try the environment
  */
-		fd = open("/etc/hostname", O_RDONLY);
-		if (fd >= 0) {
-			r = read(fd, xbuf, MAXHOSTNAMELEN);
-			if (r > 0) {
-				xbuf[r] = 0;
-				foo = xbuf;
-				while (*foo) {
-					if (*foo == '\r' || *foo == '\n')
-						*foo = 0;
-					foo++;
-				}
-				foo = xbuf;
-			}
-			close(fd);
+    fd = open("/etc/hostname", O_RDONLY);
+    if (fd >= 0) {
+	r = read(fd, xbuf, MAXHOSTNAMELEN);
+	if (r > 0) {
+	    xbuf[r] = 0;
+	    foo = xbuf;
+	    while (*foo) {
+		if (*foo == '\r' || *foo == '\n') {
+		    *foo = 0;
+		    break;
 		}
-#if 0
+		++foo;
+	    }
+	    foo = xbuf;
 	}
-#endif
-	if (!foo)
-		foo = getenv("HOSTNAME");
-
-  /* Changed by Guido Flohr: Warn if buffer was too small.  */
-  real_length = strlen (foo ? foo : "unknown");
-  
-	strncpy(buf, foo ? foo : "unknown", len < MAXHOSTNAMELEN  ? 
-	                                        len : MAXHOSTNAMELEN);
-	
-	if (real_length > len) {
-	  errno = ENAMETOOLONG;
-	  return -1;
-	}
-	
-	return 0;
+	close(fd);
+    }
+    
+    if (!foo)
+	foo = getenv("HOSTNAME");
+    
+    /* Changed by Guido Flohr: Warn if buffer was too small.  */
+    real_length = strlen (foo ? foo : "unknown");
+    
+    strncpy(buf, foo ? foo : "unknown", len < MAXHOSTNAMELEN  ? 
+	    len : MAXHOSTNAMELEN);
+    
+    if (real_length > len) {
+	__set_errno (ENAMETOOLONG);
+	return -1;
+    }
+    
+    return 0;
 }
+weak_alias (__gethostname, gethostname)
 
 /* New stubs function:  sethostname.  */
 int
-sethostname (name, length)
-     const char* name;
-     size_t length;
+__sethostname (name, length)
+    const char* name;
+    size_t length;
 {
-  errno = ENOSYS;
-  return -1;
+    __set_errno (ENOSYS);
+    return -1;
 }
+weak_alias (__sethostname, sethostname)
+
