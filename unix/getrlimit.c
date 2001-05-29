c3c0bcb0a0e84994aa7c24d84e18fa71dd44ac1d
Frank Naumann <fnaumann@boerde.de>
2001-05-29 12:54:21 +0000

    Added RLIMIT_OPEN_FILES fallback to sysconf().

--- unix/getrlimit.c
+++ unix/getrlimit.c
@@ -1,44 +1,47 @@
 /* get resource limits */
 /* written by Eric R. Smith and placed in the public domain */
 
-#ifdef __TURBOC__
-# include <sys\types.h>
-# include <sys\resource.h>
-# include <sys\time.h>
-#else
-# include <sys/types.h>
-# include <sys/resource.h>
-# include <sys/time.h>
-#endif
-
-#include <mintbind.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <mint/mintbind.h>
+
+long __sysconf (int var);
 
 int
-__getrlimit(kind, rl)
-	enum __rlimit_resource kind;
-	struct rlimit *rl;
+__getrlimit (enum __rlimit_resource kind, struct rlimit *rl)
 {
 	long limit;
 	int mode;
 
-	
 	switch(kind) {
-	case RLIMIT_CPU:
-		mode = 1;
-		break;
-	case RLIMIT_RSS:
-		mode = 2;
-		break;
-	case RLIMIT_DATA:
-		mode = 3;
-		break;
-	default:
-		__set_errno (EINVAL);
-		return -1;
+		case RLIMIT_CPU:
+			mode = 1;
+			break;
+		case RLIMIT_RSS:
+			mode = 2;
+			break;
+		case RLIMIT_DATA:
+			mode = 3;
+			break;
+		case RLIMIT_STACK:
+		case RLIMIT_FSIZE:
+		case RLIMIT_CORE:
+			__set_errno (EINVAL);
+			return -1;
+		/* fake it until the kernel catch up */
+		case RLIMIT_OPEN_FILES:
+			rl->rlim_cur = 
+			rl->rlim_max = __sysconf (_SC_OPEN_MAX);
+			return 0;
+		default:
+			__set_errno (EINVAL);
+			return -1;
 	}
+
 	limit = Psetlimit(mode, -1L);
-		
 	if (limit < 0) {
 		__set_errno (-limit);
 		return -1;
@@ -51,5 +54,4 @@ __getrlimit(kind, rl)
 	rl->rlim_max = RLIM_INFINITY;
 	return 0;
 }
-
 weak_alias (__getrlimit, getrlimit)
