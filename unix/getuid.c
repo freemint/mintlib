7e58d2789b28ae87ed38bc0069185ab4fae265a9
Guido Flohr <guido.flohr@cantanea.com>
2001-09-30 22:46:50 +0000

    fixed cast problem

--- unix/getuid.c
+++ unix/getuid.c
@@ -11,18 +11,9 @@ extern uid_t __uid;
 uid_t
 __getuid (void)
 {
-	static short have_getuid = 1;
-
-	if (have_getuid) {
-		long r;
-		
-		r = Pgetuid ();
-		if (r == -ENOSYS)
-			have_getuid = 0;
-		else
-			return (uid_t) r;
-	}
-	
-	return __uid;
+	if (__mint)
+		return Pgetuid ();
+	else
+		return __uid;
 }
 weak_alias (__getuid, getuid)
