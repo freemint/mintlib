955004167692b70e57cfe57af3143c80848b4215
Standa Opichal <opichals@gmail.com>
2003-08-19 14:38:44 +0000

    Fchown() syscall semantics changed to follow symlinks.
    This is now in sync with the libc specification.
    
    The mintlib falls back to Fchown() if the new Fchown16() syscall
    is not present (older kernel is in use).

--- mintlib/do_chown16.c
+++ mintlib/do_chown16.c
@@ -17,23 +17,25 @@ __do_chown16 (const char *_name, uid_t uid, gid_t gid, int follow_links)
 	char *name = (char *) _name;
 	int r;
 
-	if (_name == NULL)
-	  {
-	    __set_errno (EFAULT);
-	    return -1;
-	  }
-	  
-	if (!__libc_unix_names)
-	  {
-	    name = namebuf;
-	    (void)_unx2dos(_name, name, sizeof (namebuf));
-	  }
+	if (_name == NULL) {
+		__set_errno (EFAULT);
+		return -1;
+	}
+
+	if (!__libc_unix_names) {
+		name = namebuf;
+		(void)_unx2dos(_name, name, sizeof (namebuf));
+	}
+
 	r = (int)Fchown16(name, uid, gid, follow_links);
-	if (r && (r != -ENOSYS)) 
-	  {
-	    __set_errno (-r);
-	    return -1;
-	  }
+	if (r == -ENOSYS)
+		// fallback to Fchown if Fchown16 is not available
+		r = (int)Fchown(name, uid, gid);
+
+	if (r && (r != -ENOSYS)) {
+		__set_errno (-r);
+		return -1;
+	}
 	return 0;
 }
 
