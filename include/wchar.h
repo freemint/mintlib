be1d620264a991d545037f3743e4701f986f8d51
Frank Naumann <fnaumann@boerde.de>
2008-04-27 11:55:52 +0000

    Added almost empty wchar.h to ease porting of applications that include this header. Contributed by Alan Hourihane.

---
+++ include/wchar.h
@@ -0,0 +1,11 @@
+#ifndef _WCHAR_H
+#define _WCHAR_H
+
+/* Get size_t, wchar_t, wint_t and NULL from <stddef.h>.  */
+#define __need_size_t
+#define __need_wchar_t
+#define __need_NULL
+#define __need_wint_t
+#include <stddef.h>
+
+#endif /* _WCHAR_H */
