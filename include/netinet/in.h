bba470c217148532711f90541c0bd222d7888bf2
Alan Hourihane <alanh@fairlite.co.uk>
2009-01-09 19:48:07 +0000

    move definition for INET_ADDRSTRLEN

--- include/netinet/in.h
+++ include/netinet/in.h
@@ -192,10 +192,10 @@ extern const struct in6_addr in6addr_loopback;   /* ::1 */
 #define IN6ADDR_ANY_INIT { { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } } }
 #define IN6ADDR_LOOPBACK_INIT { { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 } } }
 
-#define INET_ADDRSTRLEN 16
 #define INET6_ADDRSTRLEN 46
-
 #endif
+
+#define INET_ADDRSTRLEN 16
 
 /* Get the definition of the macro to define the common sockaddr members.  */
 #include <bits/sockaddr.h>
