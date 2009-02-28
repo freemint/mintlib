3bd6198f4c25e40ec7675b518026244253e3b015
Alan Hourihane <alanh@fairlite.co.uk>
2009-02-28 17:25:07 +0000

    don't allow passing of LOCK_EX & LOCK_SH simultaneously, but flock is still broken.

--- unix/flock.c
+++ unix/flock.c
@@ -16,14 +16,21 @@ __flock (int fd, int op)
 	int cmd;
 	int retval;
 	
-	if (op & (LOCK_SH | LOCK_EX))
-		cmd = (op & LOCK_NB) ? F_TLOCK : F_LOCK;
-	else if (op & LOCK_UN)
-		cmd = F_ULOCK;
-	else {
-		__set_errno (EINVAL);
-		return -1;
-	}
+	switch (op & ~LOCK_NB) 
+	  {
+	  case LOCK_SH:
+	    cmd = (op & LOCK_NB) ? F_TLOCK : F_LOCK; /* broken, F_RDLCK */
+	    break;
+	  case LOCK_EX:
+	    cmd = (op & LOCK_NB) ? F_TLOCK : F_LOCK; /* broken, F_WRLCK */
+	    break;
+	  case LOCK_UN:
+	    cmd = F_ULOCK;
+	    break;
+	  default:
+  	    __set_errno (EINVAL);
+	    return -1;
+	  }
 	retval = __do_lock(fd, cmd, 0L, 0);
 	if (retval == -ELOCKED && (op & LOCK_NB))
 	  {
