5b4721a6a7f82156cc03812710993c6ee4f5a79b
Frank Naumann <fnaumann@boerde.de>
2009-06-04 09:18:41 +0000

    Fixed 68020+ detection. Added ColdFire detection. Rewritten getcookie.c in assembly language. Split sysvar.c into getsysvar.S and setsysvar.c Fixed MiNTLib Ident string for ColdFire. Contributed by Vincent Riviere.

--- mintlib/ident.c
+++ mintlib/ident.c
@@ -4,6 +4,9 @@ char __Ident_gnulib[] = "$Version: MiNTLib "
 #ifdef __M68020__
 "-m68020-60 "
 #endif
+#ifdef __mcoldfire__
+"-mcpu=5475 "
+#endif
 #ifdef __M68881__
 "-m68881 "
 #endif
---
+++ mintlib/setsysvar.c
@@ -0,0 +1,20 @@
+
+#include <support.h>
+#include <mint/mintbind.h>
+#include <mint/ssystem.h>
+
+#include "lib.h"
+
+void
+set_sysvar_to_long (void *var, long val)
+{
+	if(__has_no_ssystem) {
+		long save_ssp;
+
+    		save_ssp = (long) Super((void *) 0L);
+    		*((volatile long *)var) = val;
+    		(void)Super((void *) save_ssp);
+	}
+	else
+		(void) Ssystem (S_SETLVAL, var, val); /* note: root only! */
+}
