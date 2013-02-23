f84f07bf7b394f5fddd4d2643342c1e1c1cbfda5
Alan Hourihane <alanh@fairlite.co.uk>
2013-02-23 18:12:14 +0000

    Fix _SC_OPEN_MAX, and add stubs for _SC_GETPW_R_SIZE_MAX & _SC_GETGR_R_SIZE_MAX

--- include/bits/confname.h
+++ include/bits/confname.h
@@ -42,6 +42,8 @@
 #define _SC_PAGE_SIZE	6	/* pagesize */
 #define _SC_PAGESIZE	_SC_PAGE_SIZE	
 #define _SC_PHYS_PAGES	7	/* physical pages */
+#define _SC_GETPW_R_SIZE_MAX	8 /* passwd buffer size */
+#define _SC_GETGR_R_SIZE_MAX	9 /* group buffer size */
 
 #if (defined __USE_POSIX2 || defined __USE_UNIX98 \
      || defined __USE_FILE_OFFSET64 || defined __USE_LARGEFILE64 \
