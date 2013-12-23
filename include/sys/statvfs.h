beef73b9120d547e29655a1456467a0fd0cea895
Alan Hourihane <alanh@fairlite.co.uk>
2013-12-23 18:58:36 +0000

    remove unused defines

--- include/sys/statvfs.h
+++ include/sys/statvfs.h
@@ -8,16 +8,4 @@ int statvfs (const char *, struct statvfs *);
 int fstatvfs (int, struct statvfs *);
 #endif
 
-#define ST_RDONLY 1
-#define ST_NOSUID 2
-#define ST_NODEV  4
-#define ST_NOEXEC 8
-#define ST_SYNCHRONOUS 16
-#define ST_MANDLOCK    64
-#define ST_WRITE       128
-#define ST_APPEND      256
-#define ST_IMMUTABLE   512
-#define ST_NOATIME     1024
-#define ST_NODIRATIME  2048
-
 #endif
