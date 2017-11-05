f6fa24c44272e3d2ed6b28b3a4f241f4cd95c6d7
Miro Kropacek <miro.kropacek@gmail.com>
2017-11-06 09:44:45 +1100

    Update build.sh
    Fix the strip flags to include symbols from static functions.

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,5 +8,5 @@ make SHELL=/bin/bash CROSS=yes
 
 make SHELL=/bin/bash CROSS=yes prefix="${TMP}" install
 find "${TMP}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
-find "${TMP}" -type f \( -name '*.a' -o -name '*.o' \) -exec m68k-atari-mint-strip -S -x {} \;
+find "${TMP}" -type f \( -name '*.a' -o -name '*.o' \) -exec m68k-atari-mint-strip -S -X -w -N '.L[0-9]*' {} \;
 mkdir -p "${OUT}" && cd "${TMP}" && tar cjf "${OUT}/${PROJECT}-${VERSION}.tar.bz2" *
