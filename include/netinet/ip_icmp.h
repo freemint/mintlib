e60a6b9ba8c14e49f932a3c6919096f1f4c63eee
Thorsten Otto <admin@tho-otto.de>
2019-07-14 09:15:12 +0200

    Add some definitions to ip_icmp.h

--- include/netinet/ip_icmp.h
+++ include/netinet/ip_icmp.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ip_icmp.h	7.5 (Berkeley) 6/28/90
+ *	@(#)ip_icmp.h	8.1 (Berkeley) 6/10/93
  */
 
 /*
@@ -69,6 +69,15 @@ struct icmphdr
 };
 
 /*
+ * Internal of an ICMP Router Advertisement
+ */
+struct icmp_ra_addr
+{
+  uint32_t ira_addr;
+  uint32_t ira_preference;
+};
+
+/*
  * Structure of an icmp header.
  */
 struct icmp {
@@ -79,16 +88,35 @@ struct icmp {
 		u_char ih_pptr;			/* ICMP_PARAMPROB */
 		struct in_addr ih_gwaddr;	/* ICMP_REDIRECT */
 		struct ih_idseq {
-			n_short	icd_id;
-			n_short	icd_seq;
+			uint16_t	icd_id;
+			uint16_t	icd_seq;
 		} ih_idseq;
-		int ih_void;
+		uint32_t ih_void;
+
+	    /* ICMP_UNREACH_NEEDFRAG -- Path MTU Discovery (RFC1191) */
+	    struct ih_pmtu
+	    {
+	      uint16_t ipm_void;
+	      uint16_t ipm_nextmtu;
+	    } ih_pmtu;
+
+	    struct ih_rtradv
+	    {
+	      uint8_t irt_num_addrs;
+	      uint8_t irt_wpa;
+	      uint16_t irt_lifetime;
+	    } ih_rtradv;
 	} icmp_hun;
 #define	icmp_pptr	icmp_hun.ih_pptr
 #define	icmp_gwaddr	icmp_hun.ih_gwaddr
 #define	icmp_id		icmp_hun.ih_idseq.icd_id
 #define	icmp_seq	icmp_hun.ih_idseq.icd_seq
 #define	icmp_void	icmp_hun.ih_void
+#define	icmp_pmvoid	icmp_hun.ih_pmtu.ipm_void
+#define	icmp_nextmtu	icmp_hun.ih_pmtu.ipm_nextmtu
+#define	icmp_num_addrs	icmp_hun.ih_rtradv.irt_num_addrs
+#define	icmp_wpa	icmp_hun.ih_rtradv.irt_wpa
+#define	icmp_lifetime	icmp_hun.ih_rtradv.irt_lifetime
 	union {
 		struct id_ts {
 			n_time its_otime;
@@ -99,13 +127,15 @@ struct icmp {
 			struct ip idi_ip;
 			/* options and then 64 bits of data */
 		} id_ip;
+	    struct icmp_ra_addr id_radv;
 		u_long	id_mask;
-		char	id_data[1];
+		uint8_t	id_data[1];
 	} icmp_dun;
 #define	icmp_otime	icmp_dun.id_ts.its_otime
 #define	icmp_rtime	icmp_dun.id_ts.its_rtime
 #define	icmp_ttime	icmp_dun.id_ts.its_ttime
 #define	icmp_ip		icmp_dun.id_ip.idi_ip
+#define	icmp_radv	icmp_dun.id_radv
 #define	icmp_mask	icmp_dun.id_mask
 #define	icmp_data	icmp_dun.id_data
 };
@@ -113,7 +143,7 @@ struct icmp {
 /*
  * Lower bounds on packet lengths for various types.
  * For the error advice packets must first insure that the
- * packet is large enought to contain the returned ip header.
+ * packet is large enough to contain the returned ip header.
  * Only then can we do the check to see if 64 bits of packet
  * data have been returned, since we need to check the returned
  * ip header length.
@@ -122,8 +152,13 @@ struct icmp {
 #define	ICMP_TSLEN	(8 + 3 * sizeof (n_time))	/* timestamp */
 #define	ICMP_MASKLEN	12				/* address mask */
 #define	ICMP_ADVLENMIN	(8 + sizeof (struct ip) + 8)	/* min */
+#ifndef _IP_VHL
 #define	ICMP_ADVLEN(p)	(8 + ((p)->icmp_ip.ip_hl << 2) + 8)
 	/* N.B.: must separately check that ip_hl >= 5 */
+#else
+#define	ICMP_ADVLEN(p)	(8 + (IP_VHL_HL((p)->icmp_ip.ip_vhl) << 2) + 8)
+	/* N.B.: must separately check that header length >= 5 */
+#endif
 
 /*
  * Definition of type and code field values.
@@ -136,6 +171,16 @@ struct icmp {
 #define		ICMP_UNREACH_PORT	3		/* bad port */
 #define		ICMP_UNREACH_NEEDFRAG	4		/* IP_DF caused drop */
 #define		ICMP_UNREACH_SRCFAIL	5		/* src route failed */
+#define		ICMP_UNREACH_NET_UNKNOWN        6	/* unknown net */
+#define		ICMP_UNREACH_HOST_UNKNOWN       7	/* unknown host */
+#define		ICMP_UNREACH_ISOLATED	        8	/* src host isolated */
+#define		ICMP_UNREACH_NET_PROHIB	        9	/* net denied */
+#define		ICMP_UNREACH_HOST_PROHIB        10	/* host denied */
+#define		ICMP_UNREACH_TOSNET	        11	/* bad tos for net */
+#define		ICMP_UNREACH_TOSHOST	        12	/* bad tos for host */
+#define		ICMP_UNREACH_FILTER_PROHIB      13	/* admin prohib */
+#define		ICMP_UNREACH_HOST_PRECEDENCE    14	/* host prec vio. */
+#define		ICMP_UNREACH_PRECEDENCE_CUTOFF  15	/* prec cutoff */
 #define	ICMP_SOURCEQUENCH	4		/* packet lost, slow down */
 #define	ICMP_REDIRECT		5		/* shorter route, codes: */
 #define		ICMP_REDIRECT_NET	0		/* for network */
@@ -143,10 +188,13 @@ struct icmp {
 #define		ICMP_REDIRECT_TOSNET	2		/* for tos and net */
 #define		ICMP_REDIRECT_TOSHOST	3		/* for tos and host */
 #define	ICMP_ECHO		8		/* echo service */
+#define	ICMP_ROUTERADVERT	9		/* router advertisement */
+#define	ICMP_ROUTERSOLICIT	10		/* router solicitation */
 #define	ICMP_TIMXCEED		11		/* time exceeded, code: */
 #define		ICMP_TIMXCEED_INTRANS	0		/* ttl==0 in transit */
 #define		ICMP_TIMXCEED_REASS	1		/* ttl==0 in reass */
 #define	ICMP_PARAMPROB		12		/* ip header bad */
+#define		ICMP_PARAMPROB_OPTABSENT 1		/* req. opt. absent */
 #define	ICMP_TSTAMP		13		/* timestamp request */
 #define	ICMP_TSTAMPREPLY	14		/* timestamp reply */
 #define	ICMP_IREQ		15		/* information request */
@@ -158,20 +206,10 @@ struct icmp {
 
 #define	ICMP_INFOTYPE(type) \
 	((type) == ICMP_ECHOREPLY || (type) == ICMP_ECHO || \
+	(type) == ICMP_ROUTERADVERT || (type) == ICMP_ROUTERSOLICIT || \
 	(type) == ICMP_TSTAMP || (type) == ICMP_TSTAMPREPLY || \
 	(type) == ICMP_IREQ || (type) == ICMP_IREQREPLY || \
 	(type) == ICMP_MASKREQ || (type) == ICMP_MASKREPLY)
-
-#ifdef __USE_MISC
-/*
- * Internal of an ICMP Router Advertisement
- */
-struct icmp_ra_addr
-{
-  uint32_t ira_addr;
-  uint32_t ira_preference;
-};
-#endif
 
 __END_DECLS
 
