5e6cc9c21320468a770fdd806330948486628cf8
Thorsten Otto <admin@tho-otto.de>
2019-09-21 07:30:20 +0200

    Fix definitions of bswap_32 for -mshort

--- include/bits/byteswap.h
+++ include/bits/byteswap.h
@@ -23,26 +23,28 @@
 #ifndef _BITS_BYTESWAP_H
 #define _BITS_BYTESWAP_H 1
 
+#include <bits/types.h>
+
 /* Swap bytes in 16 bit value.  We don't provide an assembler version
    because GCC is smart enough to generate optimal assembler output, and
    this allows for better cse.  */
 #define __bswap_constant_16(x) \
   ((((x) >> 8) & 0xffu) | (((x) & 0xffu) << 8))
 
-static __inline unsigned short int
-__bswap_16 (unsigned short int __bsx)
+static __inline __uint16_t
+__bswap_16 (__uint16_t __bsx)
 {
   return __bswap_constant_16 (__bsx);
 }
 
 /* Swap bytes in 32 bit value.  */
 #define __bswap_constant_32(x) \
-  ((((x) & 0xff000000u) >> 24) | (((x) & 0x00ff0000u) >>  8) |		      \
-   (((x) & 0x0000ff00u) <<  8) | (((x) & 0x000000ffu) << 24))
+  ((((x) & 0xff000000ul) >> 24) | (((x) & 0x00ff0000ul) >>  8) |		      \
+   (((x) & 0x0000ff00ul) <<  8) | (((x) & 0x000000fful) << 24))
 
 #if !defined(__mcoldfire__)
-static __inline unsigned int
-__bswap_32 (unsigned int __bsx)
+static __inline __uint32_t
+__bswap_32 (__uint32_t __bsx)
 {
   if (__builtin_constant_p (__bsx))
     return __bswap_constant_32 (__bsx);
@@ -53,8 +55,8 @@ __bswap_32 (unsigned int __bsx)
   return __bsx;
 }
 #else
-static __inline unsigned int
-__bswap_32 (unsigned int __bsx)
+static __inline __uint32_t
+__bswap_32 (__uint32_t __bsx)
 {
   return __bswap_constant_32 (__bsx);
 }
@@ -74,13 +76,13 @@ __bswap_32 (unsigned int __bsx)
    | (((x) & 0x00000000000000ffull) << 56))
 
 /* Swap bytes in 64 bit value.  */
-static __inline unsigned long long
-__bswap_64 (unsigned long long __bsx)
+static __inline __uint64_t
+__bswap_64 (__uint64_t __bsx)
 {
   if (__builtin_constant_p (__bsx))
     return __bswap_constant_64 (__bsx);
   return (__bswap_32 (__bsx >> 32)
-	  | ((unsigned long long) __bswap_32 (__bsx) << 32));
+	  | ((__uint64_t) __bswap_32 (__bsx) << 32));
 }
 #endif
 
