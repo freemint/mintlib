754d9f580d4468ce0e09a39f0ccd667211110cf9
David Galvez <dgalvez75@gmail.com>
2020-01-02 11:28:02 +0100

    Correct return values for Fpipe() and Prusage()
    Thanks to Gerhard Stoll and Lonny Pursell for pointing this out.

--- unix/times.c
+++ unix/times.c
@@ -28,7 +28,7 @@ __times (struct tms *buf)
 
 	if (__mint != 0) {
 		long int r[8];
-		(void) Prusage (r);
+		(long) Prusage (r);
 		buf->tms_utime = (r[1] * CLOCKS_PER_SEC) / 1000;
 		buf->tms_stime = (r[0] * CLOCKS_PER_SEC) / 1000;
 		buf->tms_cutime = (r[3] * CLOCKS_PER_SEC) / 1000;
