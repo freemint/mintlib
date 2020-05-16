9baca57d1daf13c04c8d4931fe1aabfa531b3a7b
Miro Kropacek <miro.kropacek@gmail.com>
2020-05-16 11:37:29 +0200

    Use prefix instead of DESTDIR
    This makes possible to install mintlib into a different folder than "/usr".

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,7 +3,8 @@
 # -u: Treat unset variables as an error when substituting.
 # -x: Display expanded script commands
 
-make SHELL=/bin/bash && make SHELL=/bin/bash DESTDIR="${INSTALL_DIR}" install
+make SHELL=/bin/bash
+make SHELL=/bin/bash prefix="${INSTALL_DIR}" install
 
 find "${INSTALL_DIR}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
 find "${INSTALL_DIR}" -type f \( -name '*.a' -o -name '*.o' \) -exec m68k-atari-mint-strip -S -X -w -N '.L[0-9]*' {} \;
