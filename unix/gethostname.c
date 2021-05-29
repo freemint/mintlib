619382f6e555eeca61f63011685e36cd76fe3f80
Thorsten Otto <admin@tho-otto.de>
2021-05-29 14:50:49 +0200

    Simplify gethostname()

--- unix/gethostname.c
+++ unix/gethostname.c
@@ -42,7 +42,7 @@ __gethostname (char *buf, size_t len)
 
 	/* fall back to the old method */	
 	{
-		char *foo = 0;
+		const char *foo = 0;
 		char xbuf[MAXHOSTNAMELEN + 1];
 		int fd, r;
 		size_t real_length;
@@ -54,32 +54,35 @@ __gethostname (char *buf, size_t len)
 		if (fd >= 0) {
 			r = read(fd, xbuf, MAXHOSTNAMELEN);
 			if (r > 0) {
+				char *p;
+
 				xbuf[r] = 0;
-				foo = xbuf;
-				while (*foo) {
-					if (*foo == '\r' || *foo == '\n') {
-						*foo = 0;
+				p = xbuf;
+				while (*p) {
+					if (*p == '\r' || *p == '\n') {
+						*p = 0;
 						break;
 					}
-					++foo;
+					++p;
 				}
 				foo = xbuf;
 			}
 			close(fd);
 		}
 
-		if (!foo)
+		if (foo == NULL)
 			foo = getenv("HOSTNAME");
+		if (foo == NULL)
+			foo = "unknown";
 
 		/* Changed by Guido Flohr: Warn if buffer was too small.  */
-		real_length = strlen (foo ? foo : "unknown");
+		real_length = strlen(foo);
 
 #if __GNUC_PREREQ(8, 0)
 #pragma GCC diagnostic ignored "-Wstringop-truncation"
 #endif
 
-		strncpy(buf, foo ? foo : "unknown", len < MAXHOSTNAMELEN  ? 
-			len : MAXHOSTNAMELEN);
+		strncpy(buf, foo, len < MAXHOSTNAMELEN ? len : MAXHOSTNAMELEN);
 
 		if (real_length > len) {
 			__set_errno (ENAMETOOLONG);
