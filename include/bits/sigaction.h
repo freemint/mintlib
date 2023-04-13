626f518bc76df2f1155d0d53be1a1bae1f3c50eb
Thorsten Otto <admin@tho-otto.de>
2023-04-13 20:10:08 +0200

    Add missing SA_NODEFER value to sigaction.h

--- include/bits/sigaction.h
+++ include/bits/sigaction.h
@@ -28,7 +28,23 @@
 #define SIG_SETMASK	2
 
 #define SA_NOCLDSTOP   0x0001       /* don't send SIGCHLD when they stop */
+#define SA_NODEFER     0x0010       /* don't mask the signal we're delivering */
 #define SA_RESETHAND   0x8000       /* Reset to SIG_DFL on entry to handler. */
+
+#ifdef __USE_MISC
+/* Some aliases for the SA_ constants.  */
+# define SA_NOMASK    SA_NODEFER
+# define SA_ONESHOT   SA_RESETHAND
+#endif
+
+#if 0
+ /* NYI: */
+#define SA_NOCLDWAIT   0x0002		/* do not generate zombies on unwaited child */
+#define SA_SIGINFO     0x0040		/* Invoke signal-catching function with three arguments instead of one. */
+#define SA_ONSTACK     0x2000		/* take signal on signal stack */
+# define SA_STACK     SA_ONSTACK
+#define SA_RESTART     0x4000		/* restart system on signal return */
+#endif
 
 struct sigaction {
 	__sighandler_t 	sa_handler;	/* Pointer to signal handler.  */
