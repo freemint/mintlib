3a8a4088e997344d8ed9f8d4d579a03ac7f95dc1
Thorsten Otto <admin@tho-otto.de>
2023-05-24 14:41:54 +0200

    Implement rand_r()

---
+++ stdlib/rand_r.c
@@ -0,0 +1,51 @@
+/* Reentrant random function from POSIX.1c.
+   Copyright (C) 1996-2023 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <stdlib.h>
+
+
+/* This algorithm is mentioned in the ISO C standard, here extended
+   for 32 bits.  */
+int
+rand_r (unsigned int *seed)
+{
+  unsigned long next = *seed;
+  long result;
+
+  next *= 1103515245L;
+  next += 12345;
+  result = (next / 65536) % 2048;
+
+  next *= 1103515245L;
+  next += 12345L;
+  result <<= 10;
+  result ^= (next / 65536) % 1024;
+
+  next *= 1103515245L;
+  next += 12345L;
+  result <<= 10;
+  result ^= (next / 65536) % 1024;
+
+  *seed = next;
+
+#ifdef __MSHORT__
+  result &= RAND_MAX;
+#endif
+  return (int) result;
+}
+
