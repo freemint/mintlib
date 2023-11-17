72667bf88c04f501fe1e451b124f6ad318e46a88
Thorsten Otto <admin@tho-otto.de>
2023-11-17 10:32:16 +0100

    Move ctime() to separate file
    localtime() is referenced by do_fstat(), which is always (indirectly)
    referenced by the startup code. Having ctime defined in the same file
    will also always pull in asctime & strftime.

---
+++ tz/ctime.c
@@ -0,0 +1,22 @@
+#include "private.h"
+
+/*
+** Section 4.12.3.2 of X3.159-1989 requires that
+**	The ctime function converts the calendar time pointed to by timer
+**	to local time in the form of a string. It is equivalent to
+**		asctime(localtime(timer))
+*/
+char *ctime(const time_t *timep)
+{
+	struct tm *tmp = localtime(timep);
+
+	return tmp ? asctime(tmp) : NULL;
+}
+
+char *ctime_r(const time_t *timep, char *buf)
+{
+	struct tm mytm;
+	struct tm *tmp = localtime_r(timep, &mytm);
+
+	return tmp ? asctime_r(tmp, buf) : NULL;
+}
--- tz/localtime.c
+++ tz/localtime.c
@@ -1939,27 +1939,6 @@ static struct tm *timesub(const time_t *timep, int_fast32_t offset, struct state
 }
 
 /*
-** Section 4.12.3.2 of X3.159-1989 requires that
-**	The ctime function converts the calendar time pointed to by timer
-**	to local time in the form of a string. It is equivalent to
-**		asctime(localtime(timer))
-*/
-char *ctime(const time_t *timep)
-{
-	struct tm *tmp = localtime(timep);
-
-	return tmp ? asctime(tmp) : NULL;
-}
-
-char *ctime_r(const time_t *timep, char *buf)
-{
-	struct tm mytm;
-	struct tm *tmp = localtime_r(timep, &mytm);
-
-	return tmp ? asctime_r(tmp, buf) : NULL;
-}
-
-/*
 ** Adapted from code provided by Robert Elz, who writes:
 **	The "best" way to do mktime I think is based on an idea of Bob
 **	Kridle's (so its said...) from a long time ago.
