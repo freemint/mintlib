6a67088fa71a9a688b4ae63d5f8c3e2fe4e81dc3
Thorsten Otto <admin@tho-otto.de>
2024-04-26 11:13:13 +0200

    Fix tfork() when compiled without optimizations

--- mintlib/thread.c
+++ mintlib/thread.c
@@ -24,7 +24,7 @@
 #include <support.h>
 #include "lib.h"
 
-#define SIZE 4096L
+#define THREAD_STACKSIZE 4096L
 
 
 /* this is used by wait() and wait3() to retrieve the child's exit code */
@@ -33,12 +33,22 @@ long __waitval = -ENOENT;
 /* and this is used to retrieve the child's time */
 long __waittime = 0;
 
-static void __CDECL
-startup(register BASEPAGE *b)
+/*
+ * This function must not be compiled without optimization,
+ * because we change the stack, and the BASEPAGE parameter
+ * is not accessible afterwards.
+ */
+#pragma GCC optimize "-Os"
+#pragma GCC optimize "-fomit-frame-pointer"
+
+static void __CDECL startup(BASEPAGE *b)
 {
-	register int (*func)(long);
-	register long arg;
-	_setstack(((char *)b) + SIZE);
+	int (*func)(long);
+	long arg;
+	BASEPAGE *parent;
+
+	parent = b->p_parent;
+	_setstack(b->p_hitpa);
 	func = (int (*)(long))b->p_dbase;
 	arg = b->p_dlen;
 
@@ -46,39 +56,43 @@ startup(register BASEPAGE *b)
 	 * own copy of the environment, right?
 	 */
 	Mfree(b->p_env);
-	b->p_env = _base->p_env;
+	b->p_env = parent->p_env;
 
 	/* copy from parents basepage for debuggers... */
-	b->p_tbase = _base->p_tbase;
-	b->p_tlen = _base->p_tlen;
-	b->p_dbase = _base->p_dbase;
-	b->p_dlen = _base->p_dlen;
-	b->p_bbase = _base->p_bbase;
-	b->p_blen = _base->p_blen;
+	b->p_tbase = parent->p_tbase;
+	b->p_tlen = parent->p_tlen;
+	b->p_dbase = parent->p_dbase;
+	b->p_dlen = parent->p_dlen;
+	b->p_bbase = parent->p_bbase;
+	b->p_blen = parent->p_blen;
 
 	Pterm((*func)(arg));
+	__builtin_unreachable();
 }
 
 /* use long instead of int so vfork works OK with -mshort */
 long
 tfork(int (*func)(long), long arg)
 {
-	register BASEPAGE *b;
-	register long pid;
-	register long savpending, savmask;
-	register BASEPAGE *savbase;
+	BASEPAGE *b;
+	long pid;
+	long savpending, savmask;
 	sighandler_t savhandler[NSIG];
 	long now;
 	int i;
+	long stacksize = THREAD_STACKSIZE;
 
 	b = (BASEPAGE *)Pexec(PE_CBASEPAGE, 0L, "", 0L);
-	(void)Mshrink(b, SIZE+256);
+	if ((long)b <= 0)
+		return (long)b;
+	(void)Mshrink(b, stacksize + sizeof(*b));
 	b->p_tbase = (char *)startup;
 	b->p_dbase = (char *)func;
 	b->p_dlen = arg;
-	b->p_hitpa = ((char *)b) + SIZE + 256;
- 
-	pid = Pexec(104, 0L, b, 0L);
+	b->p_blen = stacksize;
+	b->p_hitpa = ((char *)b) + stacksize + sizeof(*b);
+
+	pid = Pexec(PE_ASYNC_GO, 0L, b, 0L);
 	if (pid == -ENOSYS)
 	{
 		/* save the signal masks and signal handlers,
@@ -90,30 +104,35 @@ tfork(int (*func)(long), long arg)
 		_sigmask = 0;
 		for (i = 0; i < NSIG; i++)
 			savhandler[i] = _sig_handler[i];
-		savbase = _base;
-		_base = b;
 
 		now = _clock();
-		pid = Pexec(4, 0L, b, 0L);
+		pid = Pexec(PE_GO, 0L, b, 0L);
 
-		_base = savbase;
 		/* restore signal stuff */
 		for (i = 0; i < NSIG; i++)
 			_sig_handler[i] = savhandler[i];
 		_sigmask = savmask;
 		_sigpending = savpending;
-		if (pid >= 0) {
+		if (pid >= 0)
+		{
 			long retval = pid;
 
-		/* see the TOS algorithm for getpid() */
+			/* see the TOS algorithm for getpid() */
 			pid = ((long)b) >> 8;
 			__waitval = (pid << 16) | retval;
 			raise(SIGCHLD);
 			__waittime = _clock() - now;
 			_childtime += __waittime;
+			/* child already terminated, but memory of basepage & env is still owned by us */
+			(void)Mfree(b->p_env);	/* free the memory */
+			(void)Mfree(b);
 		}
 	}
-	(void)Mfree(b->p_env);	/* free the memory */
-	(void)Mfree(b);
+	if (pid < 0)
+	{
+		(void)Mfree(b->p_env);	/* free the memory */
+		(void)Mfree(b);
+	}
+	/* memory of basepage & env is now owned by child */
 	return pid;
 }
