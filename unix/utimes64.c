9c10bff6e872a61159b682860b26f3d82931ac03
Thorsten Otto <admin@tho-otto.de>
2025-07-20 17:56:18 +0200

    Implement utimes64()

---
+++ unix/utimes64.c
@@ -0,0 +1,33 @@
+/*  utimes.c -- MiNTLib.
+    Copyright (C) 1999 Guido Flohr <guido@freemint.de>
+
+    This file is part of the MiNTLib project, and may only be used
+    modified and distributed under the terms of the MiNTLib project
+    license, COPYMINT.  By continuing to use, modify, or distribute
+    this file you indicate that you have read the license and
+    understand and accept it fully.
+*/
+
+#include <errno.h>
+#include <utime.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "lib.h"
+
+int __utimes64(const char *filename, const struct timeval64 tvp[2])
+{
+	struct utimbuf64 utimbuf;
+
+	if (filename == NULL) {
+		__set_errno (EFAULT);
+		return -1;
+	}
+
+	if (tvp == NULL)
+		return __utime64 (filename, NULL);
+
+	utimbuf.actime = tvp[0].tv_sec;
+	utimbuf.modtime = tvp[1].tv_sec;
+
+	return __utime64(filename, &utimbuf);
+}
