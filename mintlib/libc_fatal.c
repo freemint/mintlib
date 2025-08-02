cd729b930ef7de46397c52088617460fdca0980c
Thorsten Otto <admin@tho-otto.de>
2025-08-02 18:17:16 +0200

    Avoid calling library functions in __libc_fatal
    These might call __libc_fatal recursively
    
    Also fixes a long standing bug, using sizeof(ptr) when writing the
    message.

--- mintlib/libc_fatal.c
+++ mintlib/libc_fatal.c
@@ -12,32 +12,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <mint/osbind.h>
 #include "lib.h"
+
+#ifdef __OPTIMIZE__
+#pragma GCC optimize "-Os"
+#endif
 
 /* Abort with an error message.
    This function should write MESSAGE out in the most reliable way.
    It is called in situations like internal stdio lossage.  */
 
 void
-__libc_fatal (register const char* message)
+__libc_fatal (const char* message)
 {
 	size_t msglen = strlen (message);
-	register size_t i;
-	char* crlf = "\r\n";
-	char* colon = ": fatal libc error: ";
+	size_t i;
+	static char const crlf[] = "\r\n";
+	static char const colon[] = ": fatal libc error: ";
   
-	__write (STDERR_FILENO, program_invocation_name, 
-      		 strlen (program_invocation_name));
-  	__write (STDERR_FILENO, colon, sizeof colon - 1);
+	Fwrite(STDERR_FILENO, strlen (program_invocation_name), program_invocation_name);
+  	Fwrite(STDERR_FILENO, sizeof(colon) - 1, colon);
   
   	/* We better write a CRLF sequence here.  Of course this will make
      	   the function slower but we want the user to be able to read
      	   the message under all circumstances.  */
   	for (i = 0; i < msglen; i++) {
     		if (message[i] == '\n') 
-      			__write (STDERR_FILENO, crlf, 2);
+      			Fwrite(STDERR_FILENO, sizeof(crlf) - 1, crlf);
     		else
-      			__write (STDERR_FILENO, (char*) message + i, 1);
+      			Fwrite(STDERR_FILENO, 1, message + i);
   	}
   
   	abort ();
